69d17753444afc28af450adb9d2a05bf01b4609a
Jan Remeš <xremes00@stud.fit.vutbr.cz>
2015-12-29 15:25:36 +0100

    event/listener.php: Fixed whitespace and typo

--- event/listener.php
+++ event/listener.php
@@ -6,7 +6,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
-	/** @var \phpbb\config\config */	
+	/** @var \phpbb\config\config */
 	protected $config;
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
@@ -17,39 +17,47 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\user */
 	protected $user;
 	protected $root_path;
-	
+
 	protected $phpEx;
-/** 
- 	* Constructor 
- 	* 
- 	* @param \phpbb\config\config   		$config             	 Config object 
- 	* @param \phpbb\db\driver\driver_interface      $db        	 	 DB object 
- 	* @param \phpbb\template\template    		$template  	 	 Template object 
- 	* @param \phpbb\auth\auth      			$auth           	 Auth object 
- 	* @param \phpbb\use		     		$user           	 User object 
- 	* @param	                		$root_path          	 Root Path object 
- 	* @param                  	     		$phpEx          	 phpEx object 
- 	* @return \staffit\toptentopics\event\listener 
- 	* @access public 
- 	*/ 
-public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\template\template $template, \phpbb\auth\auth $auth, \phpbb\user $user, $root_path, $phpEx) 
-{
-   $this->config = $config;
-   $this->db = $db;
-   $this->template = $template; 
-   $this->auth = $auth;
-   $this->user = $user;
-   $this->root_path = $root_path;
-   $this->phpEx   = $phpEx ;
-}
+
+	/**
+	 * Constructor
+	 *
+	 * @param \phpbb\config\config   		$config             	 Config object
+	 * @param \phpbb\db\driver\driver_interface      $db        	 	 DB object
+	 * @param \phpbb\template\template    		$template  	 	 Template object
+	 * @param \phpbb\auth\auth      			$auth           	 Auth object
+	 * @param \phpbb\use		     		$user           	 User object
+	 * @param	                		$root_path          	 Root Path object
+	 * @param                  	     		$phpEx          	 phpEx object
+	 * @return \ciakval\vipposts\event\listener
+	 * @access public
+	 */
+	public function __construct(
+		\phpbb\config\config $config,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\template\template $template,
+		\phpbb\auth\auth $auth,
+		\phpbb\user $user,
+		$root_path,
+		$phpEx)
+	{
+		$this->config = $config;
+		$this->db = $db;
+		$this->template = $template;
+		$this->auth = $auth;
+		$this->user = $user;
+		$this->root_path = $root_path;
+		$this->phpEx   = $phpEx;
+	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.phpbb_content_visibility_get_visibility_sql_before'	=> 'limit_vip_posts',
-'core.permissions'						=> 'permissions',
-'core.posting_modify_template_vars' => 'posting_button',
-'core.submit_post_modify_sql_data' => 'posting'
+			'core.permissions'					=> 'permissions',
+			'core.posting_modify_template_vars'	=> 'posting_button',
+			'core.submit_post_modify_sql_data'	=> 'posting'
 		);
 	}
 
@@ -67,25 +75,27 @@ public function __construct(\phpbb\config\config $config, \phpbb\db\driver\drive
 
 	public function permissions($event)
 	{
-	$permissions = $event['permissions'];
-	$permissions['u_vip_view'] = array('lang' => 'ACL_U_VIP_VIEW', 'cat' => 'misc');
-	$permissions['u_vip_set'] = array('lang' => 'ACL_U_VIP_SET', 'cat' => 'misc');
-	$event['permissions'] = $permissions;
+		$permissions = $event['permissions'];
+		$permissions['u_vip_view'] = array('lang' => 'ACL_U_VIP_VIEW', 'cat' => 'misc');
+		$permissions['u_vip_set'] = array('lang' => 'ACL_U_VIP_SET', 'cat' => 'misc');
+		$event['permissions'] = $permissions;
 	}
 
 	public function posting_button($event)
 	{
-$page_data = $event['page_data'];
-$page_data['S_CAN_VIPPOST'] = $this->auth->acl_get('u_vip_post');
-$page_data['S_VIPPOST'] = ""; //checked
-$event['page_data'] = $page_data;
+		$page_data = $event['page_data'];
+		$page_data['S_CAN_VIPPOST'] = $this->auth->acl_get('u_vip_post');
+		$page_data['S_VIPPOST'] = ""; //checked
+		$event['page_data'] = $page_data;
 	}
+
 	public function posting($event)
 	{
-	$sql_data = $event['sql_data'];
-	$sql_data[POSTS_TABLE]['sql'] = array_merge($sql_data[POSTS_TABLE]['sql'], array(
-'post_vip' => $post_vip
-	);
-	$event['sql_data'] = $sql_data;
+		$sql_data = $event['sql_data'];
+		$sql_data[POSTS_TABLE]['sql'] = array_merge($sql_data[POSTS_TABLE]['sql'], array(
+			'post_vip' => $post_vip,
+		));
+
+		$event['sql_data'] = $sql_data;
 	}
 }
